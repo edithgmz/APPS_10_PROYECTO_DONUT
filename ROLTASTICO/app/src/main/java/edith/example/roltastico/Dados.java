502800817493d64d419214f7c3a476702710c5e5
Pollo <danielquintanilla9024@gmail.com>
2019-04-04 19:05:40 -0700

    los dados ya funcionan y avances en la interfaz

--- ROLTASTICO/app/src/main/java/edith/example/roltastico/Dados.java
+++ ROLTASTICO/app/src/main/java/edith/example/roltastico/Dados.java
@@ -14,29 +14,32 @@ public class Dados extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dados);
     }
-    @Override
-    public void rollDice(View view) {
-        Random random = new Random();
-        String sal;
-        Toast.makeText(this, "soy un evento, buenas tardes",Toast.LENGTH_LONG).show();
+
+
+    public void rollDice(View view) {//fallan todos los dados
+        //Random random = new Random();
+        String sal = null;
+        //Toast.makeText(this, "soy un evento, buenas tardes",Toast.LENGTH_LONG).show();
         switch (view.getId()){//el nextInt solo genera números enteros entre 0 y n, n es el parámetro
             case R.id.btn1d4://con return, viviendo al límite
-                sal =""+ random.nextInt(4)+1;//entre 0 y 4 sin llegar a 4 (osea del 0 al 3) +1 es del 1 al 4
+                sal =""+ (int)(Math.random() * 4 + 1);;//entre 0 y 4 sin llegar a 4 (osea del 0 al 3) +1 es del 1 al 4
 //número aleatorio del 1 al 4
                 break;
             case R.id.btn1d6://max)+1
-                sal= ""+ random.nextInt(6)+1;//del 0 al 6 sin llegar a 6(0-5)+ 1 es 1-6
+                sal= ""+ (int)(Math.random() * 6 + 1);;//del 0 al 6 sin llegar a 6(0-5)+ 1 es 1-6
                 //número aleatorio del 1 al 6
                 break;
-            case R.id.btn1d8
-                sal=""+ random.nextInt(8)+1; //lo mismo de antes
+            case R.id.btn1d8:
+                sal=""+ (int)(Math.random() * 8 + 1); //lo mismo de antes
                 break;
-            case R.id.btn1d20
-                sal=""+ random.nextInt(20)+1;//pero con 20
-            case R.id.btn1d100
-                sal = ""+random.nextInt(100)+1;//y 100
+            case R.id.btn1d20:
+                sal=""+ (int)(Math.random() * 20 + 1);//pero con 20
+                break;
+            case R.id.btn1d100:
+                sal = ""+(int)(Math.random() * 99 );//y 0 a 99
+                break;
         }
         //el Toast es temporal
-        Toast.makeText(this,sal,Toast.LENGTH_LONG).show();
+        Toast.makeText(this,sal,Toast.LENGTH_SHORT).show();
     }
 }
--- ROLTASTICO/app/src/main/java/edith/example/roltastico/Principal.java
+++ ROLTASTICO/app/src/main/java/edith/example/roltastico/Principal.java
@@ -1,20 +1,35 @@
 package edith.example.roltastico;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
 
-    int[] btnMenu;
+    Button[] btnMenu;
+    Intent dados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {//falta el cambio de presionar botón a otro menú
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        btnMenu= new int[]{R.id.btnRw, R.id.btnVO,//falta un sistema
-                R.id.btnDados};
+        btnMenu= new Button[]{findViewById(R.id.btnRw), findViewById(R.id.btnVO),//falta un sistema
+                findViewById(R.id.btnDados)};
+        dados = new Intent(this,  Dados.class);
+    }
 
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnDice:
+                startActivity(dados);
+                Toast.makeText(this,"cambiar pantalla",Toast.LENGTH_SHORT).show();
+                break;
+             default:
+                 Toast.makeText(this,"no está listo",Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
