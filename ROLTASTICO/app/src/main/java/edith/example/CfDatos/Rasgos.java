0aaf9861f069beda46eaf0f0dc6a295f3f5c18f4
adamio12 <daniel902447@gmail.com>
2019-05-19 18:21:06 -0600

    clases para los datos de Cf y Vo

---
+++ ROLTASTICO/app/src/main/java/edith/example/CfDatos/Rasgos.java
@@ -0,0 +1,201 @@
+package edith.example.CfDatos;
+
+public class Rasgos {
+    private int iFisico;
+    private int iBaseD;
+    private int iPelD;
+    private int iPelAr; //XD
+    private int iRes;
+    //------------
+    private int iAgilidad;
+    private int iConducir;
+    private int iPunteria;
+    private int iSigilo;
+    //------------
+    private int iInteligencia;
+    private int iMani;
+    private int iArr;
+    private int iOcEn;
+    private int iPra;
+    //------------
+    private int iEspiritu;
+    private int iInti;
+    private int iConv;
+    private int iAten;
+    private int iOrie;
+
+    public Rasgos(int iFisico, int iBaseD, int iPelD, int iPelAr, int iRes, int iAgilidad, int iConducir, int iPunteria, int iSigilo, int iInteligencia, int iMani, int iArr, int iOcEn, int iPra, int iEspiritu, int iInti, int iConv, int iAten, int iOrie) {
+        this.setiFisico(iFisico);
+        this.setiBaseD(iBaseD);
+        this.setiPelD(iPelD);
+        this.setiPelAr(iPelAr);
+        this.setiRes(iRes);
+        this.setiAgilidad(iAgilidad);
+        this.setiConducir(iConducir);
+        this.setiPunteria(iPunteria);
+        this.setiSigilo(iSigilo);
+        this.setiInteligencia(iInteligencia);
+        this.setiMani(iMani);
+        this.setiArr(iArr);
+        this.setiOcEn(iOcEn);
+        this.setiPra(iPra);
+        this.setiEspiritu(iEspiritu);
+        this.setiInti(iInti);
+        this.setiConv(iConv);
+        this.setiAten(iAten);
+        this.setiOrie(iOrie);
+    }
+
+
+    public int getiFisico() {
+        return iFisico;
+    }
+
+    public void setiFisico(int iFisico) {
+        this.iFisico = iFisico;
+    }
+
+    public int getiBaseD() {
+        return iBaseD;
+    }
+
+    public void setiBaseD(int iBaseD) {
+        this.iBaseD = iBaseD;
+    }
+
+    public int getiPelD() {
+        return iPelD;
+    }
+
+    public void setiPelD(int iPelD) {
+        this.iPelD = iPelD;
+    }
+
+    public int getiPelAr() {
+        return iPelAr;
+    }
+
+    public void setiPelAr(int iPelAr) {
+        this.iPelAr = iPelAr;
+    }
+
+    public int getiRes() {
+        return iRes;
+    }
+
+    public void setiRes(int iRes) {
+        this.iRes = iRes;
+    }
+
+    public int getiAgilidad() {
+        return iAgilidad;
+    }
+
+    public void setiAgilidad(int iAgilidad) {
+        this.iAgilidad = iAgilidad;
+    }
+
+    public int getiConducir() {
+        return iConducir;
+    }
+
+    public void setiConducir(int iConducir) {
+        this.iConducir = iConducir;
+    }
+
+    public int getiPunteria() {
+        return iPunteria;
+    }
+
+    public void setiPunteria(int iPunteria) {
+        this.iPunteria = iPunteria;
+    }
+
+    public int getiSigilo() {
+        return iSigilo;
+    }
+
+    public void setiSigilo(int iSigilo) {
+        this.iSigilo = iSigilo;
+    }
+
+    public int getiInteligencia() {
+        return iInteligencia;
+    }
+
+    public void setiInteligencia(int iInteligencia) {
+        this.iInteligencia = iInteligencia;
+    }
+
+    public int getiMani() {
+        return iMani;
+    }
+
+    public void setiMani(int iMani) {
+        this.iMani = iMani;
+    }
+
+    public int getiArr() {
+        return iArr;
+    }
+
+    public void setiArr(int iArr) {
+        this.iArr = iArr;
+    }
+
+    public int getiOcEn() {
+        return iOcEn;
+    }
+
+    public void setiOcEn(int iOcEn) {
+        this.iOcEn = iOcEn;
+    }
+
+    public int getiPra() {
+        return iPra;
+    }
+
+    public void setiPra(int iPra) {
+        this.iPra = iPra;
+    }
+
+    public int getiEspiritu() {
+        return iEspiritu;
+    }
+
+    public void setiEspiritu(int iEspiritu) {
+        this.iEspiritu = iEspiritu;
+    }
+
+    public int getiInti() {
+        return iInti;
+    }
+
+    public void setiInti(int iInti) {
+        this.iInti = iInti;
+    }
+
+    public int getiConv() {
+        return iConv;
+    }
+
+    public void setiConv(int iConv) {
+        this.iConv = iConv;
+    }
+
+    public int getiAten() {
+        return iAten;
+    }
+
+    public void setiAten(int iAten) {
+        this.iAten = iAten;
+    }
+
+    public int getiOrie() {
+        return iOrie;
+    }
+
+    public void setiOrie(int iOrie) {
+        this.iOrie = iOrie;
+    }
+}
